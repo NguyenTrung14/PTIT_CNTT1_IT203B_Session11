8c5f90ca517431fbd6cf1cd72668eddeb88ddadc
NguyenTrung14 <nguyentrung27092006@gmail.com>
2026-03-23 09:05:56 +0700

    btvn

--- src/ex5/DBContext.java
+++ src/ex5/DBContext.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBContext {
-    private static final String URL = "jdbc:mysql://localhost:3306/rikkei_care";
+    private static final String URL = "jdbc:mysql://localhost:3306/hospital_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "123456789";
+    private static final String PASSWORD = "nguyentrung243";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
--- src/ex6/DatabaseConnection.java
+++ src/ex6/DatabaseConnection.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/medicalappointmentdb";
     private static final String USER = "root";
-    private static final String PASSWORD = "123456789";
+    private static final String PASSWORD = "nguyentrung243";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
